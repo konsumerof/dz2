d559ee85da84a186ca436f392d7eaebb66a948bf
konsumerof <nive54@gmail.com>
2021-12-23 18:57:28 +0300

    123

---
+++ cmake-build-debug/CMakeFiles/dz2.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/dz2.dir/main.cpp.obj"
+  "dz2.exe"
+  "dz2.exe.manifest"
+  "dz2.pdb"
+  "libdz2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/dz2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
